71eb2134c33d971d9e66733fb68ec2c8d8a4b699
RyuSenshi <114537651+PandW18@users.noreply.github.com>
2025-03-13 14:32:32 -0400

    Funcionô

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const $html = document.querySelector('html');
 const $i = document.querySelector('i');
 
 function darkMode() {
-$html.addEventListener('click', () => {
-  $i.classList.toggle('fa-moon');
   $i.classList.toggle('fa-sun');
-});
-$i.addEventListener('change', function() {
-    $html.classList.toggle('dark-mode');
-  });
+  $html.classList.toggle('dark-mode');
 }
