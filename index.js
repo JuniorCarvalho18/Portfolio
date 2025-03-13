15f6beedda95c9e084c5a55ef4bdfd6812b0d7fa
RyuSenshi <114537651+PandW18@users.noreply.github.com>
2025-03-13 14:25:48 -0400

    add js + darkmode

---
+++ index.js
@@ -0,0 +1,10 @@
+const $html = document.querySelector('html');
+const $i = document.querySelector('i');
+
+$html.addEventListener('click', () => {
+  $i.classList.toggle('fa-moon');
+  $i.classList.toggle('fa-sun');
+});
+$i.addEventListener('change', function() {
+    $html.classList.toggle('dark-mode');
+  });
