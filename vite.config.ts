b1e8d9eeb7589da74110983be70ee1d256314434
Junior Carvalho <laerciojrcarvalho18@outlook.com>
2026-01-17 23:56:13 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio/",
+
   server: {
     host: "::",
     port: 8080,
@@ -15,7 +17,7 @@ export default defineConfig(({ mode }) => ({
       usePolling: true,
     },
   },
-  plugins: [react()],
+  plugins: [react(), mode === "development"].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
