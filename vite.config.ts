ac15932ce0be32d7d82a1d7484ad8910fdcab1d4
Junior Carvalho <laerciojrcarvalho18@outlook.com>
2026-01-18 00:17:19 -0400

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/Portfolio/",
-
   server: {
     host: "::",
     port: 8080,
@@ -17,7 +14,7 @@ export default defineConfig(({ mode }) => ({
       usePolling: true,
     },
   },
-  plugins: [react(), ...(mode === "development" ? [] : [])],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
